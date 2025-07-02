a194216e8405f5071bd27bc0b798b924aebdbaac
Artur <artur.joaquimbr@gmail.com>
2025-07-02 11:05:44 -0300

    feat: Adicionar html e estilos de página home

--- src/flaskrc/controllers/HomeController.py
+++ src/flaskrc/controllers/HomeController.py
@@ -1,12 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask_login import login_required
 
 bp = Blueprint("home", __name__, url_prefix="/")
 
 @bp.route("/", methods=["GET"])
-@login_required
+#@login_required
 def home() -> str:
     """
         Rota para página inicial.
     """
-    return "Bem-vindo à página inicial do sistema de controle de estoque!"
+    return render_template("index.html")
