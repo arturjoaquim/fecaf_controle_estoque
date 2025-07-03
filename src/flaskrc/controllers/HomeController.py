8f262faa46fe00eaf22d80ead2a88460c05bf339
Artur <artur.joaquimbr@gmail.com>
2025-07-03 18:38:03 -0300

    Adicionar listagem de produtos com estoque critico a tela inicial

--- src/flaskrc/controllers/HomeController.py
+++ src/flaskrc/controllers/HomeController.py
@@ -1,12 +1,26 @@
+from typing import TYPE_CHECKING
+
 from flask import Blueprint, render_template
 from flask_login import login_required
 
+from flaskrc.commons.mappers.ProdutoDTOMapper import ProdutoDTOMapper
+from flaskrc.repositories.ProdutoRepository import ProdutoRepository
+from flaskrc.services.produto.ConsultarProdutoService import ConsultarProdutoService
+
+if TYPE_CHECKING:
+    from flaskrc.commons.dtos.ProdutoDTO import ProdutoDTO
+
 bp = Blueprint("home", __name__, url_prefix="/")
+consultar_produto_service = ConsultarProdutoService(ProdutoRepository())
+
 
 @bp.route("/", methods=["GET"])
-#@login_required
+@login_required
 def home() -> str:
     """
         Rota para página inicial.
     """
-    return render_template("index.html")
+    produto_dto_mapper:ProdutoDTOMapper = ProdutoDTOMapper(many=True)
+    produtos: list[ProdutoDTO] = consultar_produto_service\
+        .consultar_produtos_abaixo_estoque_minimo()
+    return render_template("index.html", produtos=produto_dto_mapper.dump(produtos))
